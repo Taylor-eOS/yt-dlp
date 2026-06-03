399e11e04fd5a293d04e56d57b6275031fcc73fe
Taylor <78652522+Taylor-eOS@users.noreply.github.com>
2026-06-03 07:23:26 +0200

    Enter format

--- run.py
+++ run.py
@@ -1,12 +1,28 @@
 import yt_dlp
 
-options = {
-    #'format': '134+bestaudio',
-    #'format': '140',
-    'format': 'bestaudio',
-    #'postprocessors': [{'key': 'FFmpegVideoConvertor', 'preferedformat': 'mp4',}],
-    'outtmpl': '%(title)s.%(ext)s',
-}
-with yt_dlp.YoutubeDL(options) as ydl:
-    input_file = input("Input url: ")
-    ydl.download([input_file])
+default = 'bestaudio'
+
+def get_url():
+    return input("Input url: ").strip()
+
+def list_formats(url):
+    with yt_dlp.YoutubeDL({'listformats': True}) as ydl:
+        ydl.extract_info(url, download=False)
+
+def get_format():
+    print("Combinations like 137+140 are possible.")
+    format = input(f"Enter format (" + default + "): ").strip() or default
+    return format
+
+def download(url, fmt):
+    with yt_dlp.YoutubeDL({'format': fmt, 'outtmpl': '%(title)s.%(ext)s'}) as ydl:
+        ydl.download([url])
+
+def main():
+    url = get_url()
+    list_formats(url)
+    fmt = get_format()
+    download(url, fmt)
+
+if __name__ == '__main__':
+    main()
