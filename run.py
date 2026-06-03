014fc81c82afca1ac26527be6a1b1ee4849c418a
Taylor <78652522+Taylor-eOS@users.noreply.github.com>
2026-06-03 07:06:59 +0200

    Select url

--- run.py
+++ run.py
@@ -2,12 +2,11 @@ import yt_dlp
 
 options = {
     #'format': '134+bestaudio',
-    'format': '140',
-    #'postprocessors': [{
-    #    'key': 'FFmpegVideoConvertor',
-    #    'preferedformat': 'mp4',
-    #}],
+    #'format': '140',
+    'format': 'bestaudio',
+    #'postprocessors': [{'key': 'FFmpegVideoConvertor', 'preferedformat': 'mp4',}],
     'outtmpl': '%(title)s.%(ext)s',
 }
 with yt_dlp.YoutubeDL(options) as ydl:
-    ydl.download(['https://www.youtube.com/watch?v=FJ3N_2r6R-o'])
+    input_file = input("Input url: ")
+    ydl.download([input_file])
